1bbba61d7f6bc08f90d5413dfa004b960f660042
Brooke <brst4163exc@colorado.edu>
2020-05-27 16:59:40 -0600

    updating routes and styles

--- modules/approve_urls/src/Controller/ApproveURLController.php
+++ modules/approve_urls/src/Controller/ApproveURLController.php
@@ -4,32 +4,78 @@ namespace Drupal\approve_urls\Controller;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use \GuzzleHttp\Exception\RequestException;
 use Drupal\node\NodeInterface;
-use Drupal\node\Entity\Node;
 use Drupal\user\Entity\User;
 
-class ApproveURLController{	
+class ApproveURLController{
+    private $yourls_base_url, $yourls_secret;
+    // get the configuration settings for the YOURLs install
+    public function __construct(){
+        $config = \Drupal::config('drupal_yourls.settings');
+        $this->yourls_base_url = $config->get('yourls_url');
+        $this->yourls_secret = $config->get('yourls_secret'); 
+    }
+
+    // $nid is upcasted from the dynamic route in the routing.yml file
+    // https://api.drupal.org/api/drupal/core%21modules%21node%21src%21NodeInterface.php/interface/NodeInterface/8.2.x
     public function deleteApp(Request $req, NodeInterface $nid = null){
-        // delete the node from YOURLS and from Drupal
-        // $nid is from the dynamic route from the routing.yml
-        \Drupal::logger('approve_urls')->notice("Deleting this entry: {$nid->getTitle()}");
-        // then redirect back to the View
-        // $req is from the view and has a param that has the url to go back to
-        $viewRoute = $req->query->get('destination');
-        // redirect back to the view page once the node's been deleted
-        return new RedirectResponse($viewRoute, 302);
+        try{
+            // delete the node from YOURLS (if exists) and from Drupal
+            \Drupal::logger('approve_urls')->notice("Deleting this entry: {$nid->id()}");
+            // $yourls_api = "{$this->yourls_base_url}?signature={$this->yourls_secret}&action=delete&shorturl={}&format=json";
+            // \Drupal::httpClient()->get($yourls_api);
+            // $res = json_decode($res, true);
+            // $nid->delete();
+        }
+        catch(RequestException $e){
+            // If it gets here, the short URL to delete doesn't exist - returns a 404
+            \Drupal::logger('approve_urls')->error($e);
+        }
+        finally{
+            $viewRoute = $req->query->get('destination');
+            return new RedirectResponse($viewRoute, 302); // redirect back to the view
+        }
     }
 
     public function approveApp(Request $req, NodeInterface $nid = null){
-        $viewRoute = $req->query->get('destination');
-        // redirect back to the view page once the node's been deleted
-        return new RedirectResponse($viewRoute, 302);
+        try{
+            \Drupal::logger('approve_urls')->notice("Getting node value: {$nid->get('field_ucb_short_url')->value}");
+            // change the node's status to published and approved
+            // $nid->set();
+            // $nid->set();
+            // $nid->save();
+            // // generate a new custom short URL
+            // $long_url = $nid->get()->value;
+            // $keyword = $nid->get()->value;
+            // $title = $nid->get()->value;
+            // $yourls_api = "{$this->yourls_base_url}?signature={$this->yourls_secret}&action=shorturl&format=json&url={}&title={}&keyword={}";
+            // \Drupal::httpClient()->get($yourls_api);
+            // $res = json_decode($res, true);
+            // TODO: notify the user somehow
+        }
+        catch(\Exception $e){
+            \Drupal::logger('approve_urls')->error($e);
+        }
+        finally{
+            $viewRoute = $req->query->get('destination');
+            return new RedirectResponse($viewRoute, 302); //return to the view
+        }
     }
 
     public function rejectApp(Request $req, NodeInterface $nid = null){
-        $viewRoute = $req->query->get('destination');
-        // redirect back to the view page once the node's been deleted
-        return new RedirectResponse($viewRoute, 302);
+        try{
+            //Reject the application
+            $nid->set();
+            $nid->save();
+            // TODO: Figure out what to do about rejected applications 
+        }
+        catch(\Exception $e){
+            // do something
+        }
+        finally{
+            $viewRoute = $req->query->get('destination');
+            return new RedirectResponse($viewRoute, 302);
+        }
     }
-
 }
