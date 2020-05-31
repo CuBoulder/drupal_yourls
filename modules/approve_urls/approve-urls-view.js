69b984d8c3f7a57e0f8af801d159618d52d6adef
Brooke <brst4163exc@colorado.edu>
2020-05-31 10:33:08 -0600

    overriding the anchor tags

---
+++ modules/approve_urls/approve-urls-view.js
@@ -0,0 +1,29 @@
+(function($){
+    // Prevent the <a> tags from redirecting from the dropdown on the view
+    function handleAction(e, action, row){
+        e.preventDefault();
+        // Get the application status column - this will be updated after the fetch 
+        let status = document.querySelectorAll("td.views-field-field-ucb-url-status");
+        // make a request to the link instead of going to it
+        fetch(action)
+        .then(res => res.json())
+        .then(res =>{
+            // Change the content of the status column
+            console.log(status[row]);
+            status[row].innerText = res.action;
+            alert(res.message);
+        })
+        .catch(err => console.err(err));
+    }
+
+    $(function(){
+        let list = document.querySelectorAll('li.dropbutton-action'); //nodelist of <li>
+        let links  = Array.from(list);
+        links.forEach((i, index) => {
+            let row = Math.floor(index / 3); // find the index of the status - 3 comes from the 3 actions
+            // attach a click event to all of the <a> tags
+            // console.log(`Row : ${row}`);
+            i.children[0].addEventListener('click', e => handleAction(e, i.children[0].href, row ));
+        });
+    });
+}(jQuery));
