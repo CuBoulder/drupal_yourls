83c5001453d46f3fcb1c74e838284c936b9bc233
Brooke <brst4163exc@colorado.edu>
2020-06-03 12:06:44 -0600

    seperating short url results into block

--- modules/random_urls/randomURLs.js
+++ modules/random_urls/randomURLs.js
@@ -1,6 +1,5 @@
 (function(){
     let url = null;
-    let search = null;
     document.getElementById('new-url').addEventListener('blur', e => url = e.target.value);
     document.getElementById('shorten-button').addEventListener('click', () => {
         console.log(url);
@@ -27,24 +26,5 @@
                 m.innerText = "Something went wrong. Please try again."; 
             });
         }
-    });
-    // Handle the search feature
-    document.getElementById('next-button').addEventListener('click', event=> {
-        let next = +event.target.value + 1;
-        window.location = window.location.origin + window.location.pathname + '?page=' + next ;
-    });
-    document.getElementById('prev-button').addEventListener('click', event=> {
-        let next = +event.target.value - 1;
-        if(next < 1) next = 1;
-        window.location = window.location.origin + window.location.pathname + '?page=' + next ;
-    });
-    // handle the search form
-    document.getElementById('search-keyword').addEventListener('blur', e => {search = e.target.value});
-    document.getElementById('search-button').addEventListener('click', event => {
-        if(!search){
-            alert("Please enter a keyword to search for");
-            return;
-        }
-        window.location = window.location.origin + window.location.pathname + '?keyword=' + search;
     });
 }());
--- modules/random_urls/src/Controller/RandomURLController.php
+++ modules/random_urls/src/Controller/RandomURLController.php
@@ -59,63 +59,14 @@ class RandomURLController{
             return new Response(json_encode(['message' => $message]), Response::HTTP_OK, ['content-type' => 'application/json']);
         }
     }
-    //get all of the existing short urls from the API or data about a certain url
-    private function getAllShortURLs($page = null, $keyword=null){
-        $yourls_api = "{$this->yourls_base_url}?signature={$this->yourls_secret}&format=json";
-        // get the results of existing short URLS
-        if($page){
-            try{
-                $start = ($page * 10) - 10; //the start of results to fetch
-                $end = ($page * 10) -1; // end of the results to fetch
-                $yourls_api = "{$yourls_api}&action=stats&limit={$end}&start={$start}"; //get 10 results at a time
-                // \Drupal::logger('random_urls')->notice("url: {$yourls_api}");
-                $res = \Drupal::httpClient()->get($yourls_api);
-                $res = json_decode($res->getBody(), true);
-                return $res['links'];
-            }
-            catch(RequestException $e){
-                \Drupal::logger('random_urls')->error($e);
-                return [];
-            }
-        }
-        // get data about a specific short url
-        else if($keyword){
-            try{
-                $yourls_api = "{$yourls_api}&action=url-stats&shorturl={$keyword}";
-                $res = \Drupal::httpClient()->get($yourls_api);
-                $res = json_decode($res->getBody(), true);
-                // format the result
-                return ["link_1" => $res['link'] ];
-            }
-            catch(RequestException $e){
-                \Drupal::logger('random_urls')->error($e);
-                return []; // 404 or some other error
-            }
-        }
-        else{
-            return [];
-        }
-    }
 
     //render the page to get a random URL - Page cache is disabled for this page
-    // TODO: make this render async
-    public function render(Request $req){
-        $results = [];
-        if($req->query->get('page')){
-            $results = $this->getAllShortURLs($req->query->get('page'), null);
-        }
-        else if($req->query->get('keyword')){
-            $results = $this->getAllShortURLs(null, $req->query->get('keyword'));
-        }
-        // if no query parameters, just return the top 10 results
-        else{
-            $results = $this->getAllShortURLs(1, null);
-        }
+    public function render(){
         return(array(
             '#theme' => 'random-urls-template',
             '#yourlsBase' => $this->yourls_base_url,
             '#yourlsSecret' => $this->yourls_secret,
-            '#results' => $results
+            '#results' => []
         ));
     }
 }
