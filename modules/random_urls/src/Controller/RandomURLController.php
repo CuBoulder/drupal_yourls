777040fc32d880e457e18eabb2b7aacf5e9001ca
brookestevens <brst4163@colorado.edu>
2020-08-04 12:46:06 -0600

    changing random url to use form api

--- modules/random_urls/src/Controller/RandomURLController.php
+++ modules/random_urls/src/Controller/RandomURLController.php
@@ -4,67 +4,21 @@ namespace Drupal\random_urls\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use \GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\ClientException;
 
 
 class RandomURLController{	
-    private $yourls_base_url, $yourls_secret;
+    private $yourls_base_url, $yourls_secret, $domains;
     // get the configuration settings for the YOURLs install
     public function __construct(){
         $config = \Drupal::config('drupal_yourls.settings');
         $this->yourls_base_url = $config->get('yourls_url');
         $this->yourls_secret = $config->get('yourls_secret'); 
+        $this->domains = $config->get('yourls_allowed_domains'); 
     }
-    // check for a 404 status code
-    // returns true if the URL exists, otherwise return false
-    private function checkURLExists($url){
-        $flag = true;
-        $file_headers = @get_headers($url);
-        if(!$file_headers) $flag = false;
-        else{
-            for($i=0; $i< count($file_headers); $i++){
-                if($file_headers[$i] == 'HTTP/1.1 404 Not Found'){
-                    $flag = false;
-                    break;
-                }
-            }
-        }
-        return $flag;
-    }
-    //get a random URL from the API
-    public function getRandomURL(Request $req){
-        $url = $req->query->get('url');  //long url from user
-        // check if the domain comes from a colroado.edu site
-        preg_match('/.\.*colorado\.edu/', $url, $matches, PREG_UNMATCHED_AS_NULL); //make sure the URL comes from a colorado.edu domain
-        if(!$matches){
-            $message = "URL isn't from a colorado.edu domain.";
-            return new Response(json_encode(['message' => $message]), Response::HTTP_OK, ['content-type' => 'application/json']);
-        }
-        if($this->checkURLExists($url)){
-            // URL exists and comes from a colorado.edu domain
-            try{
-                $yourls_api = "{$this->yourls_base_url}?signature={$this->yourls_secret}&action=shorturl&format=json&url={$url}";
-                // \Drupal::logger('random_urls')->notice("url : $yourls_api");
-                $res = \Drupal::httpClient()->get($yourls_api); //call the API
-                \Drupal::logger('random_urls')->notice("Adding a new random URL. View it on your YOURLs installation");
-                $res = json_decode($res->getBody());
-                return new Response(json_encode(['message' => $res]), Response::HTTP_OK, ['content-type' => 'application/json']);
-            }
-            catch(RequestException | ClientException $e){
-                \Drupal::logger('random_urls')->error('Malformed URL or request resulted in a 404');
-                return new Response(json_encode(['message' => 'Malformed URL or request resulted in a 404']), Response::HTTP_INTERNAL_SERVER_ERROR, ['content-type' => 'application/json']);
-            }
-        }
-        else{
-            // URL doesn't exist
-            $message = "This URL can't be found. Please make sure that your URL goes somewhere.";
-            return new Response(json_encode(['message' => $message]), Response::HTTP_OK, ['content-type' => 'application/json']);
-        }
-    }
-    
-    
-    //get all of the listed URLS
+
+    //get all of the listed URLS - for the table of results
     public function getAllURLs(Request $req){
         $yourls_api = "{$this->yourls_base_url}?signature={$this->yourls_secret}&format=json";
         // get the results of existing short URLS
@@ -102,11 +56,11 @@ class RandomURLController{
 
     //render the page to get a random URL - Page cache is disabled for this page
     public function render(){
+        $form = \Drupal::formBuilder()->getForm('Drupal\random_urls\Form\RandomURLForm');
         return(array(
             '#theme' => 'random-urls-template',
-            '#yourlsBase' => $this->yourls_base_url,
-            '#yourlsSecret' => $this->yourls_secret,
-            '#results' => []
+            '#domains' => $this->domains,
+            '#form' => $form
         ));
     }
 }
