3a1bb7caf1a55547677936818b4633902b9737c2
brookestevens <brst4163@colorado.edu>
2021-03-11 20:19:04 +0000

    refactor to use dependency injection for using new service

--- modules/random_urls/src/Controller/RandomURLController.php
+++ modules/random_urls/src/Controller/RandomURLController.php
@@ -4,50 +4,48 @@ namespace Drupal\random_urls\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Exception\ClientException;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-
-class RandomURLController{	
-    private $yourls_base_url, $yourls_secret, $domains;
+class RandomURLController extends ControllerBase{
+    private $domains;
+    protected $yourls_connector;
     // get the configuration settings for the YOURLs install
-    public function __construct(){
+    public function __construct( $yourls_connector ){
         $config = \Drupal::config('drupal_yourls.settings');
-        $this->yourls_base_url = $config->get('yourls_url');
-        $this->yourls_secret = $config->get('yourls_secret'); 
-        $this->domains = $config->get('yourls_allowed_domains'); 
+        $this->domains = $config->get('yourls_allowed_domains');
+        $this->yourls_connector = $yourls_connector;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container) {
+        return new static($container->get('drupal_yourls.yourls_connector'));
     }
 
     //get all of the listed URLS - for the table of results
     public function getAllURLs(Request $req){
-        $yourls_api = "{$this->yourls_base_url}?signature={$this->yourls_secret}&format=json";
         $search = preg_match('/http/', $req->query->get('keyword')) ? 'url' : 'keyword'; // search type
         // get the results of existing short URLS
         if($req->query->get('page') !== null){
-            try{
-                $start = ($req->query->get('page') * 10) - 10; //the start of results to fetch
-                $yourls_api = "{$yourls_api}&action=stats&limit=10&start={$start}"; //get 10 results at a time (doesnt work for case senstiive keyword searches)
-                $res = \Drupal::httpClient()->get($yourls_api);
-                $res = json_decode($res->getBody(), true);
-                return new Response(json_encode(['links' => $res ]), Response::HTTP_OK, ['content-type' => 'application/json']);
+            $start = ($req->query->get('page') * 10) - 10; //the start of results to fetch
+            $res = ($this->yourls_connector)->stats($start);
+            if(isset($res['error'])){
+                return new Response(json_encode(['message' => $res['message'] ]), Response::HTTP_INTERNAL_SERVER_ERROR, ['content-type' => 'application/json']);
             }
-            catch(RequestException | ClientException $e){
-                \Drupal::logger('random_urls')->error($e->getMessage());
-                return new Response(json_encode(['message' => $e->getMessage()]), Response::HTTP_INTERNAL_SERVER_ERROR, ['content-type' => 'application/json']);
+            else{
+                return new Response(json_encode(['links' => $res ]), Response::HTTP_OK, ['content-type' => 'application/json']);
             }
         }
         // get data about a specific short url
         else if($search === 'keyword'){
-            try{
-                $yourls_api = "{$yourls_api}&action=url-stats&shorturl={$req->query->get('keyword')}";
-                $res = \Drupal::httpClient()->get($yourls_api);
-                $res = json_decode($res->getBody(), true);
-                // format the result
-                return new Response(json_encode(['links' => ['links' => [ 'link_1' => $res['link']]] ]), Response::HTTP_OK, ['content-type' => 'application/json']);
+            $res = ($this->yourls_connector)->urlStats($req->query->get('keyword'));
+            if(isset($res['error'])){
+                return new Response(json_encode(['message' => $res['message'] ]), Response::HTTP_INTERNAL_SERVER_ERROR, ['content-type' => 'application/json']);
             }
-            catch(RequestException | ClientException $e){
-                \Drupal::logger('random_urls')->error($e->getMessage());
-                return new Response(json_encode(['message' => $e->getMessage()]), Response::HTTP_INTERNAL_SERVER_ERROR, ['content-type' => 'application/json']);
+            else{
+                return new Response(json_encode(['links' => ['links' => [ 'link_1' => $res['link']]] ]), Response::HTTP_OK, ['content-type' => 'application/json']);
             }
         }
         // get data about a URL
@@ -57,25 +55,12 @@ class RandomURLController{
             if(substr($_url, -1) === '/'){
                 $_url = mb_substr($_url, 0, -1);
             }
-            try{
-                // check if this URL has already been shortened
-                $res = \Drupal::httpClient()->post($this->yourls_base_url, ['form_params' => [
-                    'action' => 'contract',
-                    'signature' => $this->yourls_secret,
-                    'url' => $_url,
-                    'format' => 'json'
-                ]]);
-                $res = json_decode($res->getBody(), true);
-                if((bool) $res['url_exists']){
-                    return new Response(json_encode(['links' => ['links' => $res['links']] ]), Response::HTTP_OK, ['content-type' => 'application/json']);
-                }
-                else{
-                    throw new Exception('URL does not exist');
-                }
+            $res = ($this->yourls_connector)->contract($_url);
+            if(!isset($res['error']) && (bool) $res['url_exists']){
+                return new Response(json_encode(['links' => ['links' => $res['links']] ]), Response::HTTP_OK, ['content-type' => 'application/json']);
             }
-            catch(Exception $e){
-                \Drupal::logger('random_urls')->error($e->getMessage());
-                return new Response(json_encode(['message' => $e->getMessage() ]), Response::HTTP_INTERNAL_SERVER_ERROR, ['content-type' => 'application/json']);
+            else{
+                return new Response(json_encode(['message' => 'URL does not exist' ]), Response::HTTP_INTERNAL_SERVER_ERROR, ['content-type' => 'application/json']);
             }
         }
         else{
