279a2abad6fd7e95b0afe31ab57fb3eab5f6055b
brookestevens <brst4163@colorado.edu>
2021-05-18 23:16:30 +0000

    add check to see if url has been shortened

--- modules/random_urls/src/Form/RandomURLForm.php
+++ modules/random_urls/src/Form/RandomURLForm.php
@@ -11,6 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class RandomURLForm extends FormBase {
     private $domains;
+    private $url_exists;
     protected $yourls_connector;
     function __construct( $yourls_connector ){
         $config = \Drupal::config('drupal_yourls.settings');
@@ -61,12 +62,24 @@ class RandomURLForm extends FormBase {
         );
         return $form;
     }
-  
     /**
     * {@inheritdoc}
     */
     public function validateForm(array &$form, FormStateInterface $form_state){
         $url = $form_state->getValue('url'); $from_approved_domain = false;
+       	// remove a trailing slash if any
+        if(substr($url, -1) === '/'){
+            $url = mb_substr($url, 0, -1);
+        }
+
+        // Check if this URL has been shortened before, random links are 1:1 while custom links are many to one
+        $res = ($this->yourls_connector)->contract( $url );
+        if(!isset($res['error'])){
+            if($res['statusCode'] == 200){
+              $this->url_exists = $res;
+            }
+        }
+
         // validate that the URL exists and comes from an approved domain
         // If the URL is a redirect, then verify that the start and end URLs are from an approved domain
         try{
@@ -89,6 +102,7 @@ class RandomURLForm extends FormBase {
         if(!$from_approved_domain){
             $form_state->setErrorByName('url', $this->t('Please make sure this URL comes from an approved domain.'));
         }
+        $form_state->setValue('url', $url);
     }
 
 
@@ -101,16 +115,19 @@ class RandomURLForm extends FormBase {
     */
     public function submitForm(array &$form, FormStateInterface $form_state) {
         $url = $form_state->getValue('url');
-        // remove a trailing slash if any
-        if(substr($url, -1) === '/'){
-            $url = mb_substr($url, 0, -1);
+        $shortened = null;
+        try{
+          if( (bool) $this->url_exists['url_exists'] === true){
+             $shortened = $this->url_exists['links']['link_1']['shorturl'];
+          }
+          else{
+            $res = ($this->yourls_connector)->shorturl($url);
+            $shortened = $res['shorturl'];
+          }
+          $form['random_url']['#markup'] = "<div role='alert' class='alert alert-success mt-2'> Your new short link is: {$shortened} </div>";
         }
-        $res = ($this->yourls_connector)->shorturl($url);
-        if(isset($res['error'])){
-            $form['random_url']['#markup'] = "<div role='alert' class='alert alert-danger mt-2'> Something went wrong trying to create this short link. Please try again. </div>";
-        }
-        else{
-            $form['random_url']['#markup'] = "<div role='alert' class='alert alert-success mt-2'> Your new short link is: {$res['shorturl']} </div>";
+        catch( \Exception $e){
+          $form['random_url']['#markup'] = "<div role='alert' class='alert alert-danger mt-2'> Something went wrong trying to create this short link. Please try again. </div>";
         }
     }
 }
